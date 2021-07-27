e5836f02fc5579dc7685d5a495dd26ab2d1019ac
Abadeer <Abadeer@bitch-bite-me>
2021-07-27 13:12:37 +0800

    Completed doReturnChromebookTest()

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -107,6 +107,7 @@ public class ResourceCentreTest {
 	public void testDoLoanChromebook() {
 		//fail("Not yet implemented");
 		// write your code here
+		
 	}
 	
 	@Test
@@ -119,6 +120,24 @@ public class ResourceCentreTest {
 	public void testDoReturnChromebook() {
 		//fail("Not yet implemented");
 		// write your code here
+		
+		//boundary
+		assertNotNull("Check if there is valid chromebook arraylist to add to", chromebookList);
+		ResourceCentre.addChromebook(chromebookList, cb1);
+		
+		//error
+		Boolean isReturned = ResourceCentre.doReturnChromebook(chromebookList, "CB0011");
+		assertFalse("Check that available chromebook CB0011 is returned - false?", isReturned);
+		
+		//normal
+		ResourceCentre.addChromebook(chromebookList, cb2);
+		cb2.setIsAvailable(false);
+		isReturned = ResourceCentre.doReturnChromebook(chromebookList, "CB0012");
+		assertTrue("Check that loaned out chrombook CB0012 is returned - true", isReturned);
+		
+		//error
+		isReturned = ResourceCentre.doReturnChromebook(chromebookList, "CB0013");
+		assertFalse("Check that non-existing chromebook CB0013 is returned - false?", isReturned);
 	}
 	
 	@After
